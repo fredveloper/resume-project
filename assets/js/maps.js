df0b24136a289ae40da9f1df08d6940cf4660479
Jose Mota <fredveloper@gmail.com>
2020-01-21 22:47:13 +0000

    JS files updated

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -15,13 +15,12 @@ function initMap() {
         { lat: 40.754932, lng: -73.984016 }
     ];
 
-    var markers = locations.map(function (location, i) {
+    var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers,
-        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
